3404ea0c7e1a883752653158f0b38fcbf6422131
furxx2000 <furxx2000@gmail.com>
2021-04-27 21:11:19 +0800

    nomad activity-detail mobile

--- activity-detail.php
+++ activity-detail.php
@@ -75,43 +75,219 @@
     </div>
 </section>
 
-<section class="activity-intro-section">
+<section class="mountain-intro-section">
     <div class="container">
         <h2 class="title ff-noto">南湖大山簡介</h2>
-        <ul class="mountain-intro flex">
-            <li class="text">
-                <p class="title ff-noto">南湖大山</p>
-                <p class="text ff-noto">
-                    南湖大山，中央山脈北段最高峰。台灣五嶽的北嶽盟主，高度上雖不及玉山、雪山、秀姑巒山，但在山友與學者的心目中，
-                    地位幾乎一致崇高，更是此生必來朝拜的百岳殿堂。
-                    有著帝王般莊嚴神聖的尊榮的主峰，引領周遭十數個山頭，形成如天際高台般的南湖地壘。
-                    過去，大量冰河曾經來過，留下最完整且多量的的冰河圈谷遺跡。
-                    到現在，南湖大山仍舊是台灣積雪最豐厚的地方，因這裡獨特的生態與地質，成為學術研究價值最高的高山地區。
-                </p>
-            </li>
-            <li class="picture"></li>
-            <li class="text">
-                <p class="title ff-noto">南湖圈谷:</p>
-                <p class="text ff-noto">
-                    南湖山區特殊的地形是由南湖主、東峰稜脊環夾，中間形成上下兩個寬闊的大圈谷，
-                    部份地質學者認為是冰河遺跡；無論登山或學術探討，南湖山區均為一重要的區域。
-                </p>
-            </li>
-            <li class="picture"></li>
-            <li class="text">
-                <p class="title ff-noto">多加屯山</p>
-                <p class="text ff-noto">
-                    位於宜蘭縣大同鄉與台中市和平區交界處，隸屬於中央山脈，南湖大山往西延伸出來的支脈尾端，
-                    是登南湖大山的第一座山頭，而在其半山稜線起點的松風嶺則以大片二葉松純林聳立，地上滿是松針舖地，週遭環境盡是松香而聞名。
-                </p>
-            </li>
-            <li class="picture"></li>
-        </ul>
+        <div class="mountain-intro-container">
+            <ul class="mountain-intro flex">
+                <li class="text">
+                    <p class="title ff-noto">南湖大山：</p>
+                    <p class="text ff-noto">
+                        南湖大山，中央山脈北段最高峰。台灣五嶽的北嶽盟主，高度上雖不及玉山、雪山、秀姑巒山，但在山友與學者的心目中，
+                        地位幾乎一致崇高，更是此生必來朝拜的百岳殿堂。
+                        有著帝王般莊嚴神聖的尊榮的主峰，引領周遭十數個山頭，形成如天際高台般的南湖地壘。
+                        過去，大量冰河曾經來過，留下最完整且多量的的冰河圈谷遺跡。
+                        到現在，南湖大山仍舊是台灣積雪最豐厚的地方，因這裡獨特的生態與地質，成為學術研究價值最高的高山地區。
+                    </p>
+                </li>
+                <li class="picture"></li>
+                <li class="text">
+                    <p class="title ff-noto">南湖圈谷：</p>
+                    <p class="text ff-noto">
+                        南湖山區特殊的地形是由南湖主、東峰稜脊環夾，中間形成上下兩個寬闊的大圈谷，
+                        部份地質學者認為是冰河遺跡；無論登山或學術探討，南湖山區均為一重要的區域。
+                    </p>
+                </li>
+                <li class="picture"></li>
+                <li class="text">
+                    <p class="title ff-noto">多加屯山：</p>
+                    <p class="text ff-noto">
+                        位於宜蘭縣大同鄉與台中市和平區交界處，隸屬於中央山脈，南湖大山往西延伸出來的支脈尾端，
+                        是登南湖大山的第一座山頭，而在其半山稜線起點的松風嶺則以大片二葉松純林聳立，地上滿是松針舖地，週遭環境盡是松香而聞名。
+                    </p>
+                </li>
+                <li class="picture"></li>
+            </ul>
+            <div class="mountain-intro-cta ff-noto">更多</div>
+        </div>
     </div>
 </section>
 
+<section class="activity-intro-section">
+    <div class="container">
+        <h2 class="title ff-noto"><span class="none">南湖大山三日</span>行程介紹</h2>
+        <div class="activity-intro-image"></div>
+        <div class="activity-daily-intro flex">
+            <div class="left">
+                <p class="text ff-noto">
+                    06：30 台北西門町集合<br>
+                    09：00 南山村 7-11<br>
+                    10：00 勝光派出所(張良橋)<br>
+                    12：30 6.7k登山口<br>
+                    14：00 多加屯山<br>
+                    15：30 木杆鞍部<br>
+                    16：00 雲稜山莊 (宿)<br>
+                     
+                </p>
+            </div>
+            <div class="right">
+                <div class="right-top ff-noto none">
+                武陵四秀指的是橫列在武陵農場北緣的四座百岳，由西至東，依序是品田山(3,524公尺)、池有山(3,303公尺)、桃山(3,325公尺)與喀拉業山(3,133公尺)，這一條長約十公里的山脈，是屬於雪山山脈向東北延伸舖展的主脊稜脈中的一段，雖然同處於同一條稜脈，但四座山峰皆有不同的特色。品田山是其中的最高峰，山頂有著以險峻聞名於岳界的「品田斷崖」；池有山是其中較平緩的山頭，山徑上有一棵造型特殊的「池有名樹」為其象徵。
+                </div>
+                <div class="right-bottom flex">
+                    <p class="text first ff-noto">第一天</p>
+                    <p class="text ff-noto">第二天</p>
+                    <p class="text ff-noto">第三天</p>
+                    <p class="text ff-noto">第四天</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<section class="guide-intro-section">
+    <div class="container">
+        <h2 class="title ff-noto">嚮導</h2>
+        <div class="guides flex">
+            <p class="text ff-noto">紫潔</p>
+            <p class="text ff-noto second">韋丞</p>
+        </div>
+        <div class="guide-intro-cards flex">
+            <div class="card">
+                <div class="guide-intro-image1"></div>
+                <div class="guide-intro-description">
+                    <p class="text ff-noto none">紫潔</p>
+                    <p class="text ff-noto">
+                        -具海外高山攀登經驗<br>
+                        -具野地救援能力<br>
+                        -具背負25KG以上能力<br>
+                        -具高山嚮導同等級以上能力<br>
+                        -具野地緊急過夜能力<br>
+                        -具戶外風險管理能力<br>
+                    </p>  
+                </div>
+            </div>
 
+            <div class="card none">
+                <div class="guide-intro-image2"></div>
+                <div class="guide-intro-description">
+                    <p class="text ff-noto">韋丞</p>
+                    <p class="text ff-noto">
+                        -具海外高山攀登經驗<br>
+                        -具野地救援能力<br>
+                        -具背負25KG以上能力<br>
+                        -具高山嚮導同等級以上能力<br>
+                        -具野地緊急過夜能力<br>
+                        -具戶外風險管理能力<br>
+                    </p>  
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="other-activity-section">
+    <div class="container">
+        <h2 class="title ff-noto">相關熱門行程</h2>
+        <div class="other-activity-image "></div>
+        <div class="other-activity-image none"></div>
+        <div class="other-activity-image none"></div>
+    </div>
+</section>
+
+<section class="comment-section">
+    <div class="container">
+        <h2 class="title ff-noto">評論</h2>
+        <div class="comment-card">
+            <div class="topBox flex">
+                <div class="user-icon">
+                    <img src="./images/User-icon/Ellipse 17.png" alt="">
+                </div>
+                <div class="rank">
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="bottomBox">
+                <p class="text ff-airbnb">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim ea quasi et id, animi numquam laudantium consequuntur nemo voluptas dolorum voluptatibus maiores libero, tempore quia facilis amet rem culpa tenetur. 
+                </p>
+            </div>
+            <div class="text ff-airbnb none">View more</div>
+        </div>
+
+        <div class="comment-card">
+            <div class="topBox flex">
+                <div class="user-icon">
+                    <img src="./images/User-icon/Ellipse 17.png" alt="">
+                </div>
+                <div class="rank">
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+
+                    <svg class="icon-star svg">
+                        <use
+                            xlink:href="./icomoon/symbol-defs.svg#icon-star"
+                        ></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="bottomBox">
+                <p class="text ff-airbnb">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim ea quasi et id, animi numquam laudantium consequuntur nemo voluptas dolorum voluptatibus maiores libero, tempore quia facilis amet rem culpa tenetur. 
+                </p>
+            </div>
+            <div class="text ff-airbnb none">View more</div>
+        </div>
+        <div class="comment-section-cta ff-noto">顯示更多</div>
+    </div>
+</section>
 
 
 <?php include __DIR__ . '/parts-php/html-scripts&footer.php'; ?>
